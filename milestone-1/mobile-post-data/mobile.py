0d07d83a447edbc8d8ce9a8dc1aa7ed9cbae4a06
miraj <mirajkhan14@gmail.com>
2022-12-24 11:19:22 +0600

    mobile post data add

---
+++ milestone-1/mobile-post-data/mobile.py
@@ -0,0 +1,29 @@
+mobile_data = {
+    'status': True,
+    'data': [
+        {'name': 'Xiaomi Note 5', 'price': '300 USD', 'made': 'China'},
+        {'name': 'Samsung Note 6', 'price': '200 USD', 'made': 'USA'},
+        {'name': 'Iphone 5', 'price': '180.5 USD', 'made': 'Japan'},
+        {'name': 'Pixel 5', 'price': '89.60 USD', 'made': 'Rusia'},
+        {'name': 'Techno 5', 'price': '110 USD', 'made': 'Uk'},
+        {'name': 'Huawei 5', 'price': '350 USD', 'made': 'Malaysia'},
+    ],
+    'exchnage_rate': 103.25
+}
+
+#  Your Code Starts from here
+mobiles = [
+        {'name': 'Xiaomi Note 5', 'price': '300 USD', 'made': 'China'},
+        {'name': 'Samsung Note 6', 'price': '200 USD', 'made': 'USA'},
+        {'name': 'Iphone 5', 'price': '180.5 USD', 'made': 'Japan'},
+        {'name': 'Pixel 5', 'price': '89.60 USD', 'made': 'Rusia'},
+        {'name': 'Techno 5', 'price': '110 USD', 'made': 'Uk'},
+        {'name': 'Huawei 5', 'price': '350 USD', 'made': 'Malaysia'},
+    ]
+exchnage_rate = 103.25
+
+for mobile in mobiles:
+    print(f'{mobile.get("name")} is made in {mobile.get("made")}. The price is {mobile.get("price")} USD which is almost equal to {mobile.get int("price" * exchnage_rate)}')
+#Xiaomi Note 5 is made in China. The price is 300 USD which is almost equal to 30975 BDT
+
+#
