fbb6a5d13e5a808a15e77c027e84e879e27e0405
miraj <mirajkhan14@gmail.com>
2022-12-24 12:41:58 +0600

    final code for mobile data

--- milestone-1/mobile-post-data/mobile.py
+++ milestone-1/mobile-post-data/mobile.py
@@ -12,18 +12,12 @@ mobile_data = {
 }
 
 #  Your Code Starts from here
-mobiles = [
-        {'name': 'Xiaomi Note 5', 'price': '300 USD', 'made': 'China'},
-        {'name': 'Samsung Note 6', 'price': '200 USD', 'made': 'USA'},
-        {'name': 'Iphone 5', 'price': '180.5 USD', 'made': 'Japan'},
-        {'name': 'Pixel 5', 'price': '89.60 USD', 'made': 'Rusia'},
-        {'name': 'Techno 5', 'price': '110 USD', 'made': 'Uk'},
-        {'name': 'Huawei 5', 'price': '350 USD', 'made': 'Malaysia'},
-    ]
-exchnage_rate = 103.25
 
-for mobile in mobiles:
-    print(f'{mobile.get("name")} is made in {mobile.get("made")}. The price is {mobile.get("price")} USD which is almost equal to {mobile.get int("price" * exchnage_rate)}')
-#Xiaomi Note 5 is made in China. The price is 300 USD which is almost equal to 30975 BDT
+for mobile in mobile_data.get('data'):
 
-#
+    name = mobile.get('name')
+    made = mobile.get('made')
+    price = mobile.get('price')
+    price_to_float = float(price.replace("USD",' '))
+    usd_to_bdt = mobile_data.get('exchnage_rate')*price_to_float
+    print(f'The {name} is made in the {made} and available for purchase at the {price}, which equivalent to approximatel {usd_to_bdt} BDT')
